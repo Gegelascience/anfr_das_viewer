e29775f93d1fe76095092205d73bb36996ee8084
gegelascience <remi.verchere2@gmail.com>
2021-12-24 18:34:56 +0100

    gestion des exceptions appel api anfr

--- helpers/anfr.py
+++ helpers/anfr.py
@@ -13,40 +13,55 @@ class AnfrConnector:
 
     def search_dataset_by_name(self, name: str):
         """recherche un dataset par son nom"""
-        response = requests.get(self.__base_url + self.__endpoint_search)
-        if response.status_code == 200:
-            if response.json().get("success"):
-                data_response: dict = response.json().get("result")
-                for res in data_response.get("results"):
-                    if res.get("name") == name:
-                        return res
+        url = self.__base_url + self.__endpoint_search
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                if response.json().get("success"):
+                    data_response: dict = response.json().get("result")
+                    for res in data_response.get("results"):
+                        if res.get("name") == name:
+                            return res
+                    return None
+                print(response.json().get("error"))
                 return None
-            print(response.json().get("error"))
+            print(response.status_code)
             return None
-        print(response.status_code)
-        return None
+        except Exception as ex:
+            print(ex)
+            return None
 
     def get_dataset_by_id(self, dataset_id: str):
         """recherche un dataset par son id"""
-        response = requests.get(self.__base_url + self.__endpoint_dataset + dataset_id)
-        if response.status_code == 200:
-            if response.json().get("success"):
-                return response.json().get("result")
+        url =self.__base_url + self.__endpoint_dataset + dataset_id
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                if response.json().get("success"):
+                    return response.json().get("result")
 
-            print(response.json().get("error"))
+                print(response.json().get("error"))
+                return None
+            print(response.status_code)
             return None
-        print(response.status_code)
-        return None
+        except Exception as ex:
+            print(ex)
+            return None
 
     def get_resource_data(self, resource_id: str,limit: int):
         """récupère une ressource par son id"""
-        response = requests.get(self.__base_url + self.__endpoint_resource + resource_id + "&limit=" + str(limit))
-        if response.status_code == 200:
-            if response.json().get("success"):
-                return response.json().get("result")
+        url =self.__base_url + self.__endpoint_resource + resource_id + "&limit=" + str(limit)
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                if response.json().get("success"):
+                    return response.json().get("result")
 
-            print(response.json().get("error"))
+                print(response.json().get("error"))
+                return None
+
+            print(response.status_code)
             return None
-
-        print(response.status_code)
-        return None
+        except Exception as ex:
+            print(ex)
+            return None
